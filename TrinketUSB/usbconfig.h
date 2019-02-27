84a15ab5cd47001920e111b26c47483b822033f8
Lawrence D'Anna <larry@elder-gods.org>
2019-02-27 13:07:50 -0800

    change usb vendor and product ids

--- TrinketUSB/usbconfig.h
+++ TrinketUSB/usbconfig.h
@@ -236,12 +236,12 @@ extern void calibrateOscillator(void);
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0x81, 0x17
+#define  USB_CFG_VENDOR_ID       0xc0, 0x16
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you may use one of obdev's free
  * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
  */
-#define  USB_CFG_DEVICE_ID       0x34, 0x24 /* = 0x05dc = 1500 */
+#define  USB_CFG_DEVICE_ID       0xdc, 0x05 /* = 0x05dc = 1500 */
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
@@ -255,8 +255,8 @@ extern void calibrateOscillator(void);
 #define USB_CFG_DEVICE_VERSION  0x00, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
-#define USB_CFG_VENDOR_NAME     'A', 'd', 'a', 'f', 'r', 'u', 'i', 't'
-#define USB_CFG_VENDOR_NAME_LEN 8
+#define USB_CFG_VENDOR_NAME     'l', 'a', 'r', 'r', 'y', '@', 'e', 'l', 'd', 'e', 'r', '-', 'g', 'o', 'd', 's', '.', 'o', 'r', 'g'
+#define USB_CFG_VENDOR_NAME_LEN 20
 /* These two values define the vendor name returned by the USB device. The name
  * must be given as a list of characters under single quotes. The characters
  * are interpreted as Unicode (UTF-16) entities.
@@ -265,8 +265,8 @@ extern void calibrateOscillator(void);
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'T', 'r', 'i', 'n', 'k', 'e', 't', ' ', 'K', 'e', 'y', 'b', 'o', 'a', 'r', 'd',
-#define USB_CFG_DEVICE_NAME_LEN 16
+#define USB_CFG_DEVICE_NAME     'G', 'a', 'r', 'a', 'g', 'e', 'D', 'o', 'o', 'r'
+#define USB_CFG_DEVICE_NAME_LEN 10
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
@@ -280,12 +280,12 @@ extern void calibrateOscillator(void);
  * to fine tune control over USB descriptors such as the string descriptor
  * for the serial number.
  */
-#define USB_CFG_DEVICE_CLASS        0x00 // 0x00 means "check the interface class instead"
+#define USB_CFG_DEVICE_CLASS        0xFF // 0xFF means "vendor defined", basically means "custom"
 #define USB_CFG_DEVICE_SUBCLASS     0x00
 /* See USB specification if you want to conform to an existing device class.
  * Class 0xff is "vendor specific".
  */
-#define USB_CFG_INTERFACE_CLASS     0x03 // 0x03 means HID
+#define USB_CFG_INTERFACE_CLASS     0x00
 #define USB_CFG_INTERFACE_SUBCLASS  0x00
 #define USB_CFG_INTERFACE_PROTOCOL  0x00
 /* See USB specification if you want to conform to an existing device class or
