7394e4a24ae00842fae119e624e34c3f44661ea8
Lawrence D'Anna <larry@elder-gods.org>
2019-02-27 16:58:05 -0800

    remove a buch of keyboard stuff

--- GarageDoor.ino
+++ GarageDoor.ino
@@ -1,28 +1,15 @@
 #include <TrinketUSB.h>
 
-long long lastTime = 0; 
-
 #define LED 1
 
 void setup()
 {
   // start USB stuff
-  TrinketKeyboard.begin();
-  lastTime = millis();
+  usbBegin();
   pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-  TrinketKeyboard.poll();
-
-//  long long now = millis();
-//  if (now - lastTime > 2000) { 
-//    //TrinketKeyboard.print("trinket");
-//    lastTime = now;
-//    digitalWrite(LED, HIGH); 
-//  }
-//  else if (now - lastTime > 300) { 
-//    digitalWrite(LED, LOW); 
-//  }
+  usbPollWrapper();
 }
--- TrinketUSB/TrinketUSB.c
+++ TrinketUSB/TrinketUSB.c
@@ -21,7 +21,7 @@ License along with TrinketKeyboard. If not, see
 */
 
 #include "cmdline_defs.h"
-#include "TrinketKeyboardC.h"
+#include "TrinketUSB.h"
 #include "usbconfig.h"
 #include "usbdrv/usbdrv.h"
 #include <avr/pgmspace.h>
