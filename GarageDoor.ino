e063156a603157060e7261eff00fe151a42b05ca
Lawrence D'Anna <larry@elder-gods.org>
2019-03-02 15:43:30 -0800

    add output pin for garage door remote

--- GarageDoor.ino
+++ GarageDoor.ino
@@ -2,12 +2,16 @@
 #include <TrinketUSB.h>
 
 #define LED 1
+#define BASE2n222 2
 
 void setup()
 {
   // start USB stuff
   usbBegin();
   pinMode(LED, OUTPUT);
+  
+  pinMode(BASE2n222, OUTPUT); 
+  digitalWrite(BASE2n222, LOW);
 
   for (int i = 0; i < 2; i++) { 
     digitalWrite(LED, HIGH);
@@ -21,4 +25,5 @@ void loop()
 {
   usbPollWrapper();
   digitalWrite(LED, usbLEDState);
+  digitalWrite(BASE2n222, usbLEDState);
 }
