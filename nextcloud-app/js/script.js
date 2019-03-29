d8b6f40ef48e0451d75583aa7dd9a1a75a738f72
Lawrence D'Anna <larry@elder-gods.org>
2019-03-28 22:31:54 -0700

    raspi door sensor stuff

--- nextcloud-app/js/script.js
+++ nextcloud-app/js/script.js
@@ -3,8 +3,22 @@
 
 $(document).ready(function () {
 
-    $('button').click(function() {
+    var update_status = function() {
+        $.ajax({
+            url: '/garagedoor',
+            method: "GET"
+        }).done(function (x) {
+            $('#status').replaceWith('<span id="status">'+x+'<span>');
+        }).fail( function() {
+            $('#status').replaceWith('<span id="status">???<span>');
+        });
+    };
 
+    update_status();
+
+    $('button.refresh').click(update_status);
+
+    $('button.click').click(function() {
         $.ajax({
             url: OC.generateUrl('/apps/garagedoor/click'),
             method: "POST"
--- nextcloud-app/templates/content/index.php
+++ nextcloud-app/templates/content/index.php
@@ -1,1 +1,31 @@
-<button>CLICK</button>
+
+<div>
+  <p>&nbsp;</p>
+</div>
+
+<div>
+  <p>&nbsp;</p>
+</div>
+
+<div id="div_status">
+  <p>
+    The garage door is currently <span id="status">XXX</span>.
+  </p>
+  <p>
+    <button class="refresh">refresh</button>
+  </p>
+</div>
+
+<div>
+  <p>&nbsp;</p>
+</div>
+<div>
+  <p>&nbsp;</p>
+</div>
+<div>
+  <p>&nbsp;</p>
+</div>
+
+<div id="div_click">
+  <button class="click">CLICK</button>
+</div>
